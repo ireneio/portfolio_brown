1b19b98a65ef7085e53535760b31ced591f4d2fe
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-07-17 21:07:50 +0800

    feat: Update layout

--- src/components/Home/Intro.tsx
+++ src/components/Home/Intro.tsx
@@ -59,7 +59,7 @@ export default function Intro({ t }: any) {
       >
         {t.intro.name}
         <motion.span
-          className="ml-2 text-4xl"
+          className="ml-2 text-2xl"
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{
