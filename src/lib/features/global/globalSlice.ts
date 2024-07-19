6111b4465e7d93f71d032b6e71b1f318e684c5ab
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-07-19 21:33:10 +0800

    feat: Add mobile image carousel

--- src/lib/features/global/globalSlice.ts
+++ src/lib/features/global/globalSlice.ts
@@ -10,10 +10,15 @@ export const globalSlice = createSlice({
   reducers: {
     setSidebarOpen: (state, action) => {
       state.sidebarOpen = action.payload;
+      function disableOnScroll(event: Event) {
+        event.preventDefault();
+      }
       if (action.payload === true) {
         document.body.style.overflowY = 'hidden'
+        window.addEventListener('scroll', disableOnScroll, { passive: false });
       } else if (action.payload === false) {
         document.body.style.overflowY = 'auto'
+        window.removeEventListener('scroll', disableOnScroll)
       }
     },
   },
